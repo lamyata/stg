3958eeda812b1c70c40e851f346e7b45d3a6e926
lamyata <muammargaddafi@abv.bg>
2016-12-02 09:26:57 +0100

    .

---
+++ cs/iTos/OperationReportBulk.cs
@@ -0,0 +1,18 @@
+﻿using iBoris.Unicorn;
+
+namespace iBoris.iTos.Operations
+{
+    public class OperationReportBulk 
+    {
+        public void Reopen(int operationReportId)
+        {
+            var operationReport = Persistence.GetPersister<OperationReport>(operationReportId).Load();
+            var registryItem = OperationRegistry.GetItem(x => x.OperationType == operationReport.Operation.Type.Description);
+            operationReportStatusChanger.Close(report);
+
+            var dynamicPersistence = new DynamicPersistence();
+            var report = (IOperationReport)dynamicPersistence.Load(registryItem.OperationReport, operationReportId);
+            dynamicPersistence.Delete(report);
+        }
+    }
+}
